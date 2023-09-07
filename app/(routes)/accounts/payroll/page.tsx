e90ffba1df369f4b411bd379ca3e03fa5706385a
Prathibha Ratnayake <prsthibha@gmail.com>
2023-09-07 19:29:48 +0530

    add empty pages

---
+++ app/(routes)/accounts/payroll/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface PayrollPageProps {
+  
+}
+
+const PayrollPage: FC<PayrollPageProps> = ({}) => {
+  return <div>PayrollPage</div>
+}
+
+export default PayrollPage

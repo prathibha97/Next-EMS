1ca0fe57f1f8c2830aed2a1f59e6c47ffa4b41d0
Prathibha Ratnayake <prsthibha@gmail.com>
2023-09-15 21:39:52 +0530

    setup pusher notifications

--- components/cards/notification-card.tsx
+++ components/cards/notification-card.tsx
@@ -15,20 +15,20 @@ import { Switch } from '@/components/ui/switch';
 import { useEffect, useState } from 'react';
 import { pusherClient } from '@/lib/pusher';
 
-const notifications = [
-  {
-    title: 'Your call has been confirmed.',
-    description: '1 hour ago',
-  },
-  {
-    title: 'You have a new message!',
-    description: '1 hour ago',
-  },
-  {
-    title: 'Your subscription is expiring soon!',
-    description: '2 hours ago',
-  },
-];
+// const notifications = [
+//   {
+//     title: 'Your call has been confirmed.',
+//     description: '1 hour ago',
+//   },
+//   {
+//     title: 'You have a new message!',
+//     description: '1 hour ago',
+//   },
+//   {
+//     title: 'Your subscription is expiring soon!',
+//     description: '2 hours ago',
+//   },
+// ];
 
 type CardProps = React.ComponentProps<typeof Card>;
 type Notification = {
@@ -59,7 +59,9 @@ export function NotificationCard({ className, ...props }: CardProps) {
     <Card className={cn('w-fit', className)} {...props}>
       <CardHeader>
         <CardTitle>Notifications</CardTitle>
-        <CardDescription>You have 3 unread messages.</CardDescription>
+        <CardDescription>
+          You have {notifications.length} unread messages.
+        </CardDescription>
       </CardHeader>
       {/* <CardContent className='grid gap-4'>
         <div className='flex items-center space-x-4 rounded-md border p-4'>
@@ -93,6 +95,18 @@ export function NotificationCard({ className, ...props }: CardProps) {
       </CardContent> */}
       <CardContent className='grid gap-4'>
         {/* ... */}
+        <div className='flex items-center space-x-4 rounded-md border p-4'>
+          <BellRing />
+          <div className='flex-1 space-y-1'>
+            <p className='text-sm font-medium leading-none'>
+              Push Notifications
+            </p>
+            <p className='text-sm text-muted-foreground'>
+              Send notifications to device.
+            </p>
+          </div>
+          <Switch />
+        </div>
         {notifications.map((notification, index) => (
           <div
             key={index}
--- lib/pusher.ts
+++ lib/pusher.ts
@@ -3,19 +3,19 @@ import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
+  cluster: 'ap2',
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(process.env.PUSHER_KEY!, {
-  cluster: process.env.PUSHER_CLUSTER!,
-  authEndpoint: '/api/pusher/auth',
-  authTransport: 'ajax',
-  auth: {
-    headers: {
-      'Content-Type': 'application/json',
+export const pusherClient = new PusherClient(
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  {
+    channelAuthorization: {
+      endpoint: '/api/pusher/auth',
+      transport: 'ajax',
     },
-  },
-});
+    cluster: 'ap2',
+  }
+);

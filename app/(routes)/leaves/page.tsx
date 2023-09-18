5d6d062af166b4b87b90b085f0bb6f2f4309759d
diduladishan <diduladishan14@gmail.com>
2023-09-18 17:13:17 +0530

    Apply for leave section table and form updated

--- app/(routes)/leaves/page.tsx
+++ app/(routes)/leaves/page.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { DataTable } from '@/components/data-table';
+"use client";
+import { DataTable } from "@/components/data-table";
 import {
   Dialog,
   DialogContent,
@@ -7,16 +7,28 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog';
-import { leaveBalanceData } from '@/constants/sample/leave-balance-data';
-import { leaveData } from '@/constants/sample/leave-data';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { FC } from 'react';
+} from "@/components/ui/dialog";
+import { leaveBalanceData } from "@/constants/sample/leave-balance-data";
+import { leaveData } from "@/constants/sample/leave-data";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
 
-import { Button } from '@/components/ui/button';
-import { columns } from './components/columns';
-import LeaveBalanceCard from './components/leave-balance-card';
+import { Button } from "@/components/ui/button";
+import { columns } from "./components/columns";
+import LeaveBalanceCard from "./components/leave-balance-card";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 interface pageProps {}
 
@@ -25,30 +37,58 @@ const page: FC<pageProps> = ({}) => {
   const { data: session } = useSession({
     required: true,
     onUnauthenticated() {
-      router.push('/');
+      router.push("/");
     },
   });
 
   return (
     <div>
-      <div className='mb-5'>
+      <div className="mb-5">
         <Dialog>
           <DialogTrigger>
-            <Button>Apply Leave</Button>
+            <Button className="bg-[#2ebdaa]">Apply Leave</Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
-              <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-              <DialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </DialogDescription>
+              <DialogTitle className="mx-48 pt-10 text-xl">
+                Add Leave
+              </DialogTitle>
+              {/* <DialogDescription>This</DialogDescription> */}
             </DialogHeader>
+            <div className="px-8 mb-4">
+              {/* <div>Leave Type</div> */}
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>Leave Type</DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>Select Leave Type</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Casual</DropdownMenuItem>
+                    <DropdownMenuItem>Annual</DropdownMenuItem>
+                    <DropdownMenuItem>Medical</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+
+              <div className="mb-2 mt-6">From</div>
+              <Input />
+              <div className="mb-2 mt-6">To</div>
+              <Input />
+              <div className="mb-2 mt-6">Duration in Days</div>
+              <Input />
+              <div className="mb-2 mt-6">Remaining leaves</div>
+              <Input />
+              <div className="mb-2 mt-6">Reason for leave</div>
+              <Textarea />
+            </div>
+            <div className="mx-auto mb-4">
+              <Button className="bg-[#2ebdaa] w-20 text-center">Submit</Button>
+            </div>
           </DialogContent>
         </Dialog>
       </div>
 
-      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 mb-5'>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 mb-5">
         {leaveBalanceData.map((leaveBalance) => (
           <LeaveBalanceCard
             key={leaveBalance.id}
@@ -61,8 +101,8 @@ const page: FC<pageProps> = ({}) => {
       <DataTable
         columns={columns}
         data={leaveData}
-        placeholder='Date'
-        searchFilter='requestDate'
+        placeholder="Date"
+        searchFilter="requestDate"
       />
     </div>
   );

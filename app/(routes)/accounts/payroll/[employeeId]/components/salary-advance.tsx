e43367e10bfab1360ad05c2d001c389102ed0a60
diduladishan <diduladishan14@gmail.com>
2023-09-20 00:05:50 +0530

    salary advance section added

--- app/(routes)/accounts/payroll/[employeeId]/components/salary-advance.tsx
+++ app/(routes)/accounts/payroll/[employeeId]/components/salary-advance.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/dialog";
 
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 interface SalaryAdvanceProps {}
 
@@ -23,11 +25,41 @@ const SalaryAdvance: FC<SalaryAdvanceProps> = ({}) => {
         </Button>
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+        <DialogHeader className="bg-[#eef5f9]">
+          {/* <DialogTitle>Are you sure absolutely sure?</DialogTitle> */}
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            <div className="bg-[#fff] m-8 rounded-lg pb-6 drop-shadow-lg">
+              <div className="p-4">
+                <Image
+                  className="mt-6"
+                  src="/icons/salary-advance.png"
+                  alt="Image 1"
+                  width={300}
+                  height={300}
+                />
+              </div>
+              <div>
+                <div className="text-2xl text-[#2ebdaa] font-medium text-center mt-6">
+                  Salary Advance
+                </div>
+                <div className="text-center my-4 text-slate-800">
+                  When an employee requires a salary advance,<br></br> the funds
+                  can be provided through this section.
+                </div>
+              </div>
+              <div>
+                <div className="flex gap-4 p-4 mb-4">
+                  <Button variant="outline">Date</Button>
+                  <Input />
+                </div>
+
+                <div className="text-center">
+                  <Button className="bg-[#2ebdaa] text-white" variant="outline">
+                    Submit
+                  </Button>
+                </div>
+              </div>
+            </div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>

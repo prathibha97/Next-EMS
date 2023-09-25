03d016b217e24906ec3043bb60fbf1a8a6ed6c11
Prathibha Ratnayake <prsthibha@gmail.com>
2023-09-25 17:09:54 +0530

    bug fix

--- app/(routes)/leaves/page.tsx
+++ app/(routes)/leaves/page.tsx
@@ -345,39 +345,39 @@ const page: FC<pageProps> = ({}) => {
 
       <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 mb-5'>
         <LeaveBalanceCard
-          balance={currentEmployee?.leaveBalance.annual}
+          balance={currentEmployee?.leaveBalance?.annual}
           entitlement={currentEmployee?.employeeType === 'fullTime' ? 7 : 0}
           leaveType='Annual'
         />
         <LeaveBalanceCard
-          balance={currentEmployee?.leaveBalance.casual}
+          balance={currentEmployee?.leaveBalance?.casual}
           entitlement={currentEmployee?.employeeType === 'fullTime' ? 7 : 1}
           leaveType='Casual'
         />
         <LeaveBalanceCard
-          balance={currentEmployee?.leaveBalance.medical}
+          balance={currentEmployee?.leaveBalance?.medical}
           entitlement={currentEmployee?.employeeType === 'fullTime' ? 7 : 1}
           leaveType='Medical'
         />
         {currentEmployee?.employeeType === 'fullTime' &&
-          currentEmployee?.leaveBalance.broughtForward > 1 && (
+          currentEmployee?.leaveBalance?.broughtForward > 1 && (
             <LeaveBalanceCard
-              balance={currentEmployee?.leaveBalance.broughtForward}
+              balance={currentEmployee?.leaveBalance?.broughtForward}
               entitlement={currentEmployee?.employeeType === 'fullTime' ? 4 : 0}
               leaveType='Brought Forward'
             />
           )}
-        {currentEmployee?.leaveBalance.duty > 1 && (
+        {currentEmployee?.leaveBalance?.duty > 1 && (
           <LeaveBalanceCard
-            balance={currentEmployee?.leaveBalance.unpaid}
-            entitlement={currentEmployee?.leaveBalance.duty}
+            balance={currentEmployee?.leaveBalance?.unpaid}
+            entitlement={currentEmployee?.leaveBalance?.duty}
             leaveType='Unpaid'
           />
         )}
-        {currentEmployee?.leaveBalance.duty > 1 && (
+        {currentEmployee?.leaveBalance?.duty > 1 && (
           <LeaveBalanceCard
-            balance={currentEmployee?.leaveBalance.duty}
-            entitlement={currentEmployee?.leaveBalance.duty}
+            balance={currentEmployee?.leaveBalance?.duty}
+            entitlement={currentEmployee?.leaveBalance?.duty}
             leaveType='Duty'
           />
         )}

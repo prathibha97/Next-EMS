0d9447fbdd187fd4fc86cbb75b02f6e0906e62cc
pasan1227 <pasanratnayake@gmail.com>
2024-01-09 19:54:33 +0530

    Profile page responsiveness added

--- app/(routes)/profile/page.tsx
+++ app/(routes)/profile/page.tsx
@@ -14,17 +14,24 @@ interface EmployeeProps {
 }
 
 const Employee: FC<EmployeeProps> = async ({ params }) => {
-  // const router = useRouter();
   const { employeeId } = params;
 
   const { getLoggedInEmployee } = useEmployee();
   const employee = await getLoggedInEmployee();
-  console.log(employee);
 
   return (
     <div className="bg-slate-50 md:w-[850px] xl:w-[950px] p-5 rounded-lg dark:bg-gray-800/40">
       <div className="border p-5 rounded-md">
-        <div className="flex justify-between">
+        <div className="flex flex-col md:flex-row items-center md:items-start">
+          <div className="mb-4 md:mr-8">
+            <Image
+              src={employee?.profile_photo || "/avatar.jpeg"}
+              alt="Image"
+              width={90}
+              height={90}
+              className="rounded-lg"
+            />
+          </div>
           <div className="flex flex-col">
             <h1 className="text-2xl font-semibold">
               {employee?.name || "Employee Name"}
@@ -33,21 +40,12 @@ const Employee: FC<EmployeeProps> = async ({ params }) => {
               {employee?.position || "Position"}
             </h2>
           </div>
-          <div>
-            <Image
-              src={employee?.profile_photo || "/prathibha.jpg"}
-              alt="Image"
-              width={90}
-              height={90}
-              className="rounded-lg mr-10"
-            />
-          </div>
         </div>
       </div>
 
-      <div className="flex flex-col justify-between mt-5 border p-5 rounded-md">
+      <div className="flex flex-col mt-5 border p-5 rounded-md">
         <h1 className="text-2xl font-semibold">Personal Information</h1>
-        <div className="flex flex-wrap mt-5 justify-between">
+        <div className="flex flex-wrap mt-5">
           <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
             <span>
               Name:{" "}
@@ -102,9 +100,9 @@ const Employee: FC<EmployeeProps> = async ({ params }) => {
 
       {/* <Separator className="mt-3" /> */}
 
-      <div className="flex flex-col justify-between mt-5 border p-5 rounded-md">
+      <div className="flex flex-col mt-5 border p-5 rounded-md">
         <h1 className="text-2xl font-semibold">Employment Details</h1>
-        <div className="flex flex-wrap mt-5 justify-between">
+        <div className="flex flex-wrap mt-5">
           <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
             <span>
               Job Title:{" "}

becaef9e42cd5fd3a9f4f6c0479a1229cafb2257
Prathibha Ratnayake <prsthibha@gmail.com>
2024-01-09 09:53:03 +0530

    bug fix - resend error

--- app/api/cron/send-deadline-notification/route.ts
+++ app/api/cron/send-deadline-notification/route.ts
@@ -1,48 +1,48 @@
-// import ProjectDeadlineReminderEmail from '@/components/emails/deadline-reminder-email';
-// import prisma from '@/lib/prisma';
-// import { NextResponse, type NextRequest } from 'next/server';
-// import { Resend } from 'resend';
+import ProjectDeadlineReminderEmail from '@/components/emails/deadline-reminder-email';
+import prisma from '@/lib/prisma';
+import { NextResponse, type NextRequest } from 'next/server';
+import { Resend } from 'resend';
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-// export async function GET(request: NextRequest) {
-//   try {
-//     const twoDaysFromNow = new Date();
-//     twoDaysFromNow.setDate(twoDaysFromNow.getDate() + 2);
-//     const projects = await prisma.project.findMany({
-//       where: {
-//         endDate: {
-//           lte: twoDaysFromNow,
-//         },
-//       },
-//       include: {
-//         client: true,
-//       },
-//     });
+export async function GET(request: NextRequest) {
+  try {
+    const twoDaysFromNow = new Date();
+    twoDaysFromNow.setDate(twoDaysFromNow.getDate() + 2);
+    const projects = await prisma.project.findMany({
+      where: {
+        endDate: {
+          lte: twoDaysFromNow,
+        },
+      },
+      include: {
+        client: true,
+      },
+    });
 
-//     if (projects.length === 0) {
-//       return NextResponse.json({
-//         success: true,
-//         message: 'No projects found.',
-//       });
-//     }
+    if (projects.length === 0) {
+      return NextResponse.json({
+        success: true,
+        message: 'No projects found.',
+      });
+    }
 
-//     // Iterate through projects and send notifications
-//     for (const project of projects) {
-//       const data = await resend.emails.send({
-//         from: 'onboarding@resend.dev',
-//         to: 'prsthibha@gmail.com',
-//         subject: 'Project Deadline Reminder',
-//         react: ProjectDeadlineReminderEmail({
-//           project,
-//         }),
-//       });
-//     }
-//     return NextResponse.json({
-//       success: true,
-//       message: 'Reminders sent successfully.',
-//     });
-//   } catch (error: any) {
-//     return NextResponse.json({ success: false, message: error.message });
-//   }
-// }
+    // Iterate through projects and send notifications
+    for (const project of projects) {
+      const data = await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'prsthibha@gmail.com',
+        subject: 'Project Deadline Reminder',
+        react: ProjectDeadlineReminderEmail({
+          project,
+        }),
+      });
+    }
+    return NextResponse.json({
+      success: true,
+      message: 'Reminders sent successfully.',
+    });
+  } catch (error: any) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}

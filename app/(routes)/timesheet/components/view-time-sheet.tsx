7eebcc75c2ef924d0a4d7c1529b5e47a9cf188de
Prathibha Ratnayake <prsthibha@gmail.com>
2024-01-16 17:07:42 +0530

    improve filtering in timesheet

--- app/(routes)/timesheet/components/view-time-sheet.tsx
+++ app/(routes)/timesheet/components/view-time-sheet.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { filterTaskWorkByMonth } from '@/lib/filterTaskWorkByMonth';
 import { TaskWorkWithTaskWithProjectWithClient } from '@/types';
 import { FC, useState } from 'react';
 import { AddTimeLogDialog } from './add-timelog';
@@ -8,15 +7,36 @@ import { TimeSheetTable } from './time-sheet-table';
 
 interface ViewTimeSheetProps {
   taskWork: TaskWorkWithTaskWithProjectWithClient[];
-  employeeId:string;
+  employeeId: string;
 }
 
-const ViewTimeSheet: FC<ViewTimeSheetProps> = ({ taskWork,employeeId }) => {
+const ViewTimeSheet: FC<ViewTimeSheetProps> = ({ taskWork, employeeId }) => {
   const [selectedMonth, setSelectedMonth] = useState<number | null>(null);
+  const [selectedYear, setSelectedYear] = useState<number | null>(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const filteredTaskWork = selectedMonth
-    ? filterTaskWorkByMonth(taskWork, selectedMonth)
-    : taskWork;
+  const filteredTaskWork = taskWork.filter((task) => {
+    if (
+      (selectedMonth === null || task.date.getMonth() + 1 === selectedMonth) &&
+      (selectedYear === null || task.date.getFullYear() === selectedYear) &&
+      (selectedDate === null ||
+        task.date.toDateString() === selectedDate.toDateString())
+    ) {
+      return true;
+    }
+    return false;
+  });
+
+  const getUniqueYears = (
+    taskWork: TaskWorkWithTaskWithProjectWithClient[]
+  ) => {
+    const uniqueYears = Array.from(
+      new Set(taskWork.map((task) => task.date.getFullYear()))
+    );
+    return uniqueYears;
+  };
+
+  const uniqueYears = getUniqueYears(taskWork);
 
   // Calculate total hours
   const totalHours = filteredTaskWork.reduce(
@@ -27,22 +47,33 @@ const ViewTimeSheet: FC<ViewTimeSheetProps> = ({ taskWork,employeeId }) => {
   return (
     <div className='bg-white dark:bg-gray-900/60 p-5 rounded-lg shadow'>
       <div className='flex justify-between'>
-        <select
-          value={selectedMonth || ''}
-          onChange={(e) => setSelectedMonth(Number(e.target.value) || null)}
-          className='p-2 px-4 rounded-lg focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 bg-slate-100 dark:bg-slate-400/60 text-sm'
-        >
-          <option value=''>All Months</option>
-          {Array.from({ length: 12 }, (_, i) => i + 1).map((month) => (
-            <option key={month} value={month}>
-              {new Date(2023, month - 1, 1).toLocaleString('default', {
-                month: 'long',
-              })}
-            </option>
-          ))}
-        </select>
+        <div>
+          <label>Sort By Year</label>
+          <select
+            value={selectedYear || ''}
+            onChange={(e) => setSelectedYear(Number(e.target.value) || null)}
+            className='p-2 px-4 rounded-lg focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 bg-slate-100 dark:bg-slate-400/60 text-sm ml-2'
+          >
+            <option value=''>All Years</option>
+            {uniqueYears.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
 
-        <AddTimeLogDialog employeeId={employeeId}/>
+        <div>
+          <label>Sort By Date</label>
+          <input
+            type='date'
+            value={selectedDate ? selectedDate.toISOString().split('T')[0] : ''}
+            onChange={(e) => setSelectedDate(new Date(e.target.value))}
+            className='p-2 px-4 rounded-lg focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 bg-slate-100 dark:bg-slate-400/60 text-sm ml-2'
+          />
+        </div>
+
+        <AddTimeLogDialog employeeId={employeeId} />
       </div>
 
       <TimeSheetTable

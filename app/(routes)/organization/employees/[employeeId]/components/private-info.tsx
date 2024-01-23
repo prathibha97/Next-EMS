74354cd16200fd85d7ed74d663cc1f6fbdeaccd8
pasan1227 <pasanratnayake@gmail.com>
2024-01-23 13:55:12 +0530

    View employee section redesigned

--- app/(routes)/organization/employees/[employeeId]/components/private-info.tsx
+++ app/(routes)/organization/employees/[employeeId]/components/private-info.tsx
@@ -10,44 +10,34 @@ interface PrivateInfoProps {
 const PrivateInfo: FC<PrivateInfoProps> = ({ employee }) => {
   return (
     <>
-      <div className='flex justify-between'>
+      <div className="flex justify-between">
         {/* Private Contact */}
         <div>
-          <h2 className='text-lg font-semibold'>Private Contact</h2>
-          <Separator className='mt-1 mb-3' />
-          <div className='flex flex-col gap-y-4'>
-            <div className='flex gap-x-2'>
+          <h2 className="text-lg font-semibold">Private Contact</h2>
+          <Separator className="mt-1 mb-3" />
+          <div className="flex flex-col gap-y-4">
+            <div className="flex gap-x-2">
               <span>Private Address: </span>
-              {/* <div className='flex flex-col'>
-                <span className='text-sm text-gray-600'>147/6 Pallewatte,</span>
-                <span className='text-sm text-gray-600'>
-                  Uduwawala, Katugastota,
-                </span>
-                <div className='flex gap-x-8 text-sm text-gray-600'>
-                  <span>Kandy</span>
-                  <span>Central</span>
-                  <span>20800</span>
-                  <span>Sri Lanka</span>
-                </div>
-              </div> */}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.privateAddress}
               </span>
             </div>
             <span>
               Personal Email :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.personalEmail}
               </span>
             </span>
             <span>
-              Phone : <span className='text-sm text-gray-600 dark:text-gray-300'>
+              Phone :{' '}
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.phone}
               </span>
             </span>
             <span>
               Bank Account Number :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.bankAccountNumber}
               </span>
             </span>
@@ -56,18 +46,18 @@ const PrivateInfo: FC<PrivateInfoProps> = ({ employee }) => {
 
         {/* Family Status */}
         <div>
-          <h2 className='text-lg font-semibold'>Family Status</h2>
-          <Separator className='mt-1 mb-3' />
-          <div className='flex flex-col gap-y-4'>
+          <h2 className="text-lg font-semibold">Family Status</h2>
+          <Separator className="mt-1 mb-3" />
+          <div className="flex flex-col gap-y-4">
             <span>
               Marital Status :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.maritalStatus}
               </span>
             </span>
             <span>
               Number of dependent children :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.numberOfDependents}
               </span>
             </span>
@@ -76,20 +66,20 @@ const PrivateInfo: FC<PrivateInfoProps> = ({ employee }) => {
       </div>
 
       {/* Emergency contacts */}
-      <div className='flex justify-between mt-5'>
+      <div className="flex justify-between mt-5">
         <div>
-          <h2 className='text-lg font-semibold'>Emergency Contact</h2>
-          <Separator className='mt-1 mb-3' />
-          <div className='flex flex-col gap-y-4'>
+          <h2 className="text-lg font-semibold">Emergency Contact</h2>
+          <Separator className="mt-1 mb-3" />
+          <div className="flex flex-col gap-y-4">
             <span>
               Contact Name :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.emergencyContactName}
               </span>
             </span>
             <span>
               Contact Number :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.emergencyContactPhone}
               </span>
             </span>
@@ -99,30 +89,31 @@ const PrivateInfo: FC<PrivateInfoProps> = ({ employee }) => {
         {/* Citizenship*/}
 
         <div>
-          <h2 className='text-lg font-semibold'>Citizenship</h2>
-          <Separator className='mt-1 mb-3' />
-          <div className='flex flex-col gap-y-4'>
+          <h2 className="text-lg font-semibold">Citizenship</h2>
+          <Separator className="mt-1 mb-3" />
+          <div className="flex flex-col gap-y-4">
             <span>
               Nationality :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.nationality}
               </span>
             </span>
             <span>
               Identification Number :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.idNumber}
               </span>
             </span>
             <span>
-              Gender : <span className='text-sm text-gray-600 dark:text-gray-300'>
+              Gender :{' '}
+              <span className="text-sm text-gray-600 dark:text-gray-300">
                 {employee?.gender}
               </span>
             </span>
             <span>
               date of Birth :{' '}
-              <span className='text-sm text-gray-600 dark:text-gray-300'>
-                {format(new Date(employee?.dateOfBirth as Date),'MM/dd/yyyy')}
+              <span className="text-sm text-gray-600 dark:text-gray-300">
+                {format(new Date(employee?.dateOfBirth as Date), 'MM/dd/yyyy')}
               </span>
             </span>
           </div>

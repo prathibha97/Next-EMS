094d1cbe04d8f3c3a7cea4a1746e1cff8b914bbf
Prathibha Ratnayake <prsthibha@gmail.com>
2024-02-02 19:44:44 +0530

    update base url

--- app/redux/services/api.ts
+++ app/redux/services/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `https://next-ems.vercel.app/api`,
+  baseUrl: `${process.env.NEXT_PUBLIC_URL}/api`,
 });
 
 export const apiSlice = createApi({

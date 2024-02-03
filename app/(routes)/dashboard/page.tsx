bdf447ee7456868183056a37e209284b841c0ab3
pasan1227 <pasanratnayake@gmail.com>
2024-02-03 10:50:04 +0530

    Homepage styles fixed

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -44,139 +44,56 @@ export default async function DashboardPage() {
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsContent value="overview" className="space-y-4">
-              {/* {session?.user.role === 'ADMIN' && (
-                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                  <Card className="bg-white drop-shadow-lg">
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                      <CardTitle className="text-sm font-medium">
-                        Total Revenue
-                      </CardTitle>
-                      <div>
-                        <Image
-                          src="/icons/revenue.png"
-                          alt="Image 1"
-                          width={50}
-                          height={50}
-                        />
-                      </div>
-                    </CardHeader>
-                    <CardContent>
-                      <div className="text-2xl font-bold">$45,231.89</div>
-                      <p className="text-xs text-muted-foreground text-teal-500">
-                        +20.1% from last month
-                      </p>
-                    </CardContent>
-                  </Card>
-
-                  <Card className="bg-white drop-shadow-lg">
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                      <CardTitle className="text-sm font-medium">
-                        Subscriptions
-                      </CardTitle>
-                      <Image
-                        src="/icons/subscriptions.png"
-                        alt="Image 1"
-                        width={50}
-                        height={50}
-                      />
-                    </CardHeader>
-                    <CardContent>
-                      <div className="text-2xl font-bold">+2350</div>
-                      <p className="text-xs text-muted-foreground text-teal-400">
-                        +180.1% from last month
-                      </p>
-                    </CardContent>
-                  </Card>
-                  <Card className="bg-white drop-shadow-lg">
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                      <CardTitle className="text-sm font-medium">
-                        Sales
-                      </CardTitle>
-                      <Image
-                        src="/icons/sales.png"
-                        alt="Image 1"
-                        width={50}
-                        height={50}
-                      />
-                    </CardHeader>
-                    <CardContent>
-                      <div className="text-2xl font-bold">+12,234</div>
-                      <p className="text-xs text-muted-foreground text-teal-400">
-                        +19% from last month
-                      </p>
-                    </CardContent>
-                  </Card>
-                  <Card className="bg-white drop-shadow-lg">
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                      <CardTitle className="text-sm font-medium">
-                        Active Now
-                      </CardTitle>
-                      <Image
-                        src="/icons/active.png"
-                        alt="Image 1"
-                        width={50}
-                        height={50}
-                      />
-                    </CardHeader>
-                    <CardContent>
-                      <div className="text-2xl font-bold">+573</div>
-                      <p className="text-xs text-muted-foreground text-teal-400">
-                        +201 since last hour
-                      </p>
-                    </CardContent>
-                  </Card>
-                </div>
-              )} */}
-
               <Card className="col-span-4 bg-white drop-shadow-lg ">
                 <CardHeader>
                   <CardTitle>Performance Insights</CardTitle>
                 </CardHeader>
                 <CardContent className="pl-2">
                   {(session?.user.role === 'ADMIN' && (
-                    <WorkedHoursOverviewAdmin
-                      workedHours={totalWorkedHours}
-                    />
+                    <WorkedHoursOverviewAdmin workedHours={totalWorkedHours} />
                   )) || <WorkedHoursOverview workedHours={workedHours} />}
                 </CardContent>
               </Card>
 
-              <div className="flex flex-col md:flex-row w-full">
-                <Card className="flex-1 md:w-1/2 bg-white drop-shadow-lg md:mr-2">
-                  <CardHeader>
-                    <CardTitle>Recent Projects</CardTitle>
-                    <CardDescription>Ongoing projects</CardDescription>
-                  </CardHeader>
-                  <CardContent className="flex-1">
-                    <RecentProjects />
-                  </CardContent>
-                </Card>
-
-                {(session?.user.role === 'ADMIN' && (
-                  <Card className="flex-1 md:w-1/2 bg-white drop-shadow-lg">
+              <div className="flex flex-col md:flex-row w-full gap-4">
+                <div>
+                  <Card className="flex-1 md:w-1/2 bg-white drop-shadow-lg md:mr-2">
                     <CardHeader>
-                      <CardTitle>Attendance</CardTitle>
-                      <CardDescription>
-                        Todays's employee attendance
-                      </CardDescription>
+                      <CardTitle>Recent Projects</CardTitle>
+                      <CardDescription>Ongoing projects</CardDescription>
                     </CardHeader>
                     <CardContent className="flex-1">
-                      <EmployeeAttendance />
+                      <RecentProjects />
                     </CardContent>
                   </Card>
-                )) || (
-                  <Card className="flex-1 md:w-1/2 bg-white drop-shadow-lg">
-                    <CardHeader>
-                      <CardTitle>Task Statistics</CardTitle>
-                      <CardDescription>
-                        Statistics of tasks allocated to you
-                      </CardDescription>
-                    </CardHeader>
-                    <CardContent className="flex-1">
-                      <TaskStatistics tasks={tasks} />
-                    </CardContent>
-                  </Card>
-                )}
+                </div>
+                <div>
+                  {(session?.user.role === 'ADMIN' && (
+                    <Card className="flex-1 md:w-1/2 bg-white drop-shadow-lg">
+                      <CardHeader>
+                        <CardTitle>Attendance</CardTitle>
+                        <CardDescription>
+                          Todays's employee attendance
+                        </CardDescription>
+                      </CardHeader>
+                      <CardContent className="flex-1">
+                        <EmployeeAttendance />
+                      </CardContent>
+                    </Card>
+                  )) || (
+                    <Card className="flex-1 md:w-1/2 bg-white drop-shadow-lg">
+                      <CardHeader>
+                        <CardTitle>Task Statistics</CardTitle>
+                        <CardDescription>
+                          Statistics of tasks allocated to you
+                        </CardDescription>
+                      </CardHeader>
+                      <CardContent className="flex-1">
+                        <TaskStatistics tasks={tasks} />
+                      </CardContent>
+                    </Card>
+                  )}
+                </div>
               </div>
             </TabsContent>
           </Tabs>

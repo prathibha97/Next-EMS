1910ab8e536afb4a62dc3ebf005ac725fb85b4ea
Prathibha Ratnayake <prsthibha@gmail.com>
2024-02-14 15:32:37 +0530

    add initial actions and db schema updates

---
+++ actions/create-board/index.ts
@@ -0,0 +1,70 @@
+'use server';
+
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { revalidatePath } from 'next/cache';
+import { CreateBoard } from './schema';
+import { InputType, ReturnType } from './types';
+import { createAuditLog } from '@/lib/create-audit-log';
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { title, image } = data;
+
+  const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+    image.split('|');
+
+  if (
+    !imageId ||
+    !imageThumbUrl ||
+    !imageFullUrl ||
+    !imageUserName ||
+    !imageLinkHTML
+  ) {
+    return {
+      error: 'Missing fields. Failed to create board.',
+    };
+  }
+
+  let board;
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHTML,
+      },
+    });
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.CREATE,
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to create',
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+  return {
+    data: board,
+  };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
---
+++ actions/delete-board/index.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import { createSafeAction } from '@/lib/create-safe-action';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { DeleteBoard } from './schema';
+import { InputType, ReturnType } from './types';
+import { createAuditLog } from '@/lib/create-audit-log';
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { id } = data;
+  let board;
+
+  try {
+    board = await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.DELETE,
+    });
+  } catch {
+    return {
+      error: 'Failed to delete.',
+    };
+  }
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+};
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler);

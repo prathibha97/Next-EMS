dae8d0f3a0a4dc1f297fc6ec530faa6588ea1493
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-19 11:27:06 +0530

    project boards bug fix

--- app/(routes)/boards/_components/project-selector.tsx
+++ app/(routes)/boards/_components/project-selector.tsx
@@ -8,23 +8,45 @@ import {
 } from '@/components/ui/select';
 import { Project } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 interface ProjectSelectorProps {
   projects: Project[];
 }
 
 export const ProjectSelector: FC<ProjectSelectorProps> = ({ projects }) => {
-  const router = useRouter()
+  const router = useRouter();
   const [selectedProject, setSelectedProject] = useState<string | null>(null);
+  const [initialLoad, setInitialLoad] = useState<boolean>(false);
+
+  const storedProjectId = localStorage.getItem('selectedProjectId');
+  useEffect(() => {
+    if (!initialLoad) {
+      if (
+        storedProjectId &&
+        projects.some((project) => project.id === storedProjectId)
+      ) {
+        setSelectedProject(storedProjectId);
+        router.replace(`/boards?projectId=${storedProjectId}`);
+      } else {
+        setSelectedProject(projects.length > 0 ? projects[0].id : null);
+      }
+      setInitialLoad(true);
+    }
+  }, [projects, initialLoad]);
 
   const handleProjectChange = (projectId: string) => {
     setSelectedProject(projectId);
+    localStorage.setItem('selectedProjectId', projectId);
     router.push(`/boards?projectId=${projectId}`);
-    router.refresh()
+    router.refresh();
   };
+
   return (
-    <Select onValueChange={(value) => handleProjectChange(value)}>
+    <Select
+      onValueChange={(value) => handleProjectChange(value)}
+      defaultValue={storedProjectId!}
+    >
       <SelectTrigger className='w-[180px]'>
         <SelectValue placeholder='Select Project' />
       </SelectTrigger>
@@ -33,7 +55,9 @@ export const ProjectSelector: FC<ProjectSelectorProps> = ({ projects }) => {
           <SelectItem
             key={project.id}
             value={project.id}
-            onClick={()=>router.push(`/boards?projectId=${project.id}`)}
+            // @ts-ignore
+            selected={selectedProject === project.id}
+            onClick={() => handleProjectChange(project.id)}
           >
             {project.name}
           </SelectItem>

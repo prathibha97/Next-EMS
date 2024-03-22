98c6a5a34dec73ddb4315129e073606ba1cf7d61
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-22 12:22:00 +0530

    bug fix - task list

--- app/(routes)/projects/tasks/page.tsx
+++ app/(routes)/projects/tasks/page.tsx
@@ -1,6 +1,8 @@
 import useTasks from '@/hooks/useTasks';
 import ViewTasks from './components/view-tasks';
 
+export const revalidate = 0;
+
 export default async function TaskPage() {
   const { getAllTasks } = useTasks();
   const tasks = await getAllTasks();
